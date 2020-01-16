fe038d4b3c462f20450ccf56629a5f833582452a
Ellis Andrews <ellisandrews1@gmail.com>
2020-01-16 00:41:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,8 +27,8 @@ end
 
 def monopoly_with_fourth_tier
   base = monopoly_with_third_tier()
-  base[:railroads][:names][:reading_railroad] = {"mortgage_value": $100}
-  base[:railroads][:names][:pennsylvania_railroad] = {"mortgage_value": $200}
-  base[:railroads][:names][:b_and_o_railroad] = {"mortgage_value": $400}
-  base[:railroads][:names][:shortline_railroad] = {"mortgage_value": $800}
+  base[:railroads][:names][:reading_railroad] = {"mortgage_value": "$100"}
+  base[:railroads][:names][:pennsylvania_railroad] = {"mortgage_value": "$200"}
+  base[:railroads][:names][:b_and_o_railroad] = {"mortgage_value": "$400"}
+  base[:railroads][:names][:shortline_railroad] = {"mortgage_value": "$800"}
 end
