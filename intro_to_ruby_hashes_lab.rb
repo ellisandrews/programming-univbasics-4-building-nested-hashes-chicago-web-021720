7f5f37e2d58e140e52881b8cdbe8bc850ffc0a8d
Ellis Andrews <ellisandrews1@gmail.com>
2020-01-16 00:42:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,4 +31,5 @@ def monopoly_with_fourth_tier
   base[:railroads][:names][:pennsylvania_railroad] = {"mortgage_value": "$200"}
   base[:railroads][:names][:b_and_o_railroad] = {"mortgage_value": "$400"}
   base[:railroads][:names][:shortline_railroad] = {"mortgage_value": "$800"}
+  base
 end
